8a46d06a180905bbe3f1932b21d95bbebfe80077
Luciano Ribeiro <luciano.ar@live.com>
2022-05-25 02:45:15 -0300

    add image for each project

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -20,6 +20,12 @@ import sass from "../../img/icons/sass.png";
 
 // project logos
 import lchat from "../../img/lchat.svg";
+import users from "../../img/users.svg";
+import clock from "../../img/clock.svg";
+import oldlr from "../../img/oldlr.svg";
+import paw from "../../img/paw.svg";
+import pencil from "../../img/pencil.svg";
+import todo from "../../img/todo.svg";
 
 function Article() {
   return (
@@ -31,42 +37,49 @@ function Article() {
             projectLink="https://luchat.netlify.app"
             logo={lchat}
             title="LCHAT"
+            about="Chat com cadastro e perfil de usuários projeto com o nome mais comprido do que o normal jeto com o nome mais comprido do que o normal jeto com o nome mais comprido do que o normal "
+            githubLink="https://github.com/luciano44/LChat"
+          />
+          <Project
+            projectLink="https://luciano44.github.io/Analog-Clock/"
+            logo={clock}
+            title="Relógio Analógico"
             about="Chat com cadastro e perfil de usuários"
             githubLink="https://github.com/luciano44/LChat"
           />
           <Project
-            projectLink="https://luchat.netlify.app"
-            logo={lchat}
-            title="LCHAT"
+            projectLink="https://luciano44.github.io/PET-photos/"
+            logo={paw}
+            title="Imagens de Animais"
+            about="Chat com cadastro e perfil de usuários lorem ipsum dolor siamet and i don't remember what's written in the rest of the lorem text"
+            githubLink="https://github.com/luciano44/LChat"
+          />
+          <Project
+            projectLink="https://luciano44.github.io/drawing-app/"
+            logo={pencil}
+            title="App de Desenho"
             about="Chat com cadastro e perfil de usuários"
             githubLink="https://github.com/luciano44/LChat"
           />
           <Project
-            projectLink="https://luchat.netlify.app"
-            logo={lchat}
-            title="LCHAT"
+            projectLink="https://luciano44.github.io/userlist/"
+            logo={users}
+            title="Listagem de Usuários"
             about="Chat com cadastro e perfil de usuários"
             githubLink="https://github.com/luciano44/LChat"
           />
           <Project
-            projectLink="https://luchat.netlify.app"
-            logo={lchat}
-            title="LCHAT"
+            projectLink="https://luciano44.github.io"
+            logo={oldlr}
+            title="Portfólio Antigo"
             about="Chat com cadastro e perfil de usuários"
             githubLink="https://github.com/luciano44/LChat"
           />
           <Project
-            projectLink="https://luchat.netlify.app"
-            logo={lchat}
-            title="LCHAT"
-            about="Chat com cadastro e perfil de usuários"
-            githubLink="https://github.com/luciano44/LChat"
-          />
-          <Project
-            projectLink="https://luchat.netlify.app"
-            logo={lchat}
-            title="LCHAT"
-            about="Chat com cadastro e perfil de usuários"
+            projectLink="https://luciano44.github.io/to-do-list/"
+            logo={todo}
+            title="Lista de Afazeres"
+            about="Visualize, adiciona e remova afazeres."
             githubLink="https://github.com/luciano44/LChat"
           />
         </ul>
