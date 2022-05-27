5a63215027a4b89392aa905a27c0864bdada8df2
Luciano Ribeiro <luciano.ar@live.com>
2022-05-27 04:27:29 -0300

    add profile picture

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./style.scss";
+import profile from "../../img/icons/profile.png";
 
 function Section() {
   return (
     <section>
       <div className="profile">
-        <img
-          src="https://64.media.tumblr.com/5df53fc42703b5b712ba00da78ef2377/f06686c9a98e095b-5f/s540x810/1f4a2feb76111ca2b15434aef6aeb6ea32e82d10.pnj"
-          alt="Avatar"
-        />
+        <img src={profile} alt="Avatar" />
         <div className="name">
           <span>Luciano Ribeiro</span>
           <div className="blue-line"></div>
